54a776a0b1cdd9cac6a61614f65de8c87ef03274
Rod Falanga <rfalanga@users.noreply.github.com>
2022-03-04 18:13:58 -0700

    added Day1 class
    To handle the Day 1 coding project

---
+++ AdventOfCode2021/AdventOfCode2021/Day1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    public class Day1
+    {
+        //Ingest the Day 1 data
+
+    }
+}
