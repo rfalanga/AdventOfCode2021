ac04feb610ada8d9516c1762079ee05aaee30aaf
Rod Falanga <rfalanga@users.noreply.github.com>
2022-03-04 18:45:01 -0700

    begun coding

--- AdventOfCode2021/AdventOfCode2021/Day1.cs
+++ AdventOfCode2021/AdventOfCode2021/Day1.cs
@@ -6,9 +6,22 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode2021
 {
-    public class Day1
+    public static class Day1
     {
-        //Ingest the Day 1 data
+        public static void DoDay1()
+        {
+            Console.WriteLine("In Day1");
 
+            // inspired by this link: https://stackoverflow.com/questions/13762338/read-files-from-a-folder-present-in-project
+            string executionPath = Environment.CurrentDirectory;
+            var path = executionPath + @"\Data\Day1.txt";
+
+            //Ingest the Day 1 data
+            using (FileStream fs = File.Open(@"Data\Day1.txt", FileMode.Open))
+            {
+                int i = 1;
+            }
+
+        }
     }
 }
