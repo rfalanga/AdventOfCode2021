2816168064f946b20d42e111a15b214447ab4f66
Rod Falanga <rfalanga@users.noreply.github.com>
2022-03-05 09:31:28 -0700

    trying to figure out what I've done wrong

--- AdventOfCode2021/AdventOfCode2021/Day2.cs
+++ AdventOfCode2021/AdventOfCode2021/Day2.cs
@@ -24,64 +24,69 @@ namespace AdventOfCode2021
             var window1 = new Models.ThreeMemberWindow();   //first window
             var window2 = new Models.ThreeMemberWindow();   //second window, always one value behind window1
 
-            //window1.Add(1);
-            //Console.WriteLine($"Sum of window1 {window1.Sum()}");
-            //window1.Add(2);
-            //Console.WriteLine($"Sum of window1 {window1.Sum()}");
-            //window1.Add(3);
-            //Console.WriteLine($"Sum of window1 {window1.Sum()}");
-            //window1.Add(4);
-            //Console.WriteLine($"Sum of window1 {window1.Sum()}");
-            //window1.Add(5);
-            //Console.WriteLine($"Sum of window1 {window1.Sum()}");
-            //window1.Add(6);
-            //Console.WriteLine($"Sum of window1 {window1.Sum()}");
+            window1.Add(1);
+            Console.WriteLine($"Sum of window1 {window1.Sum()}");
+            window1.Add(2);
+            window2.Add(2);
+            Console.WriteLine($"Sum of window1 {window1.Sum()}");
+            window1.Add(3);
+            window2.Add(3);
+            Console.WriteLine($"Sum of window1 {window1.Sum()}");
+            window1.Add(4);
+            window2.Add(4);
+            Console.WriteLine($"Sum of window1 {window1.Sum()}");
+            window1.Add(5);
+            window2.Add(5);
+            Console.WriteLine($"Sum of window1 {window1.Sum()}");
+            window1.Add(6);
+            window2.Add(6);
+            Console.WriteLine($"Sum of window1 {window1.Sum()}");
 
-            int sum1 = -1, sum2 = -1;
-            int numOfIncreases = 0;
+            //int sum1 = -1, sum2 = -1;
+            //int numOfIncreases = 0;
 
-            for (int i = 0; i < lines.Length; i++)
-            {
-                var line = lines[i];
-                int integerRead = 0;
+            //for (int i = 0; i < lines.Length; i++)
+            //{
+            //    var line = lines[i];
+            //    int integerRead = 0;
 
-                //only handle integer values
-                if (int.TryParse(line, out integerRead))
-                {
-                    window1.Add(integerRead);
-                    if (i > 0)
-                    {
-                        window2.Add(integerRead);
-                    }
+            //    //only handle integer values
+            //    if (int.TryParse(line, out integerRead))
+            //    {
+            //        window1.Add(integerRead);
+            //        if (i > 0)
+            //        {
+            //            window2.Add(integerRead);
+            //        }
 
-                    sum1 = window1.Sum();
-                    sum2 = window2.Sum();
-                    if (sum1 > 0 && sum2 == -1)
-                    {
-                        //now we can start reporting
-                        Console.WriteLine($"{sum1} (N/A - no previous sum)");
-                    }
-                    else if (sum1 > 0 && sum2 > 0)
-                    {
-                        //continue reporting
-                        if (sum1 < sum2)
-                        {
-                            Console.WriteLine($"{sum2} (decrease)");
-                        }
-                        else if (sum1 == sum2)
-                        {
-                            Console.WriteLine($"{sum2} (no change)");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"{sum2} (increased)");
-                            numOfIncreases++;
-                        }
-                    }
-                }
-            }
+            //        sum1 = window1.Sum();
+            //        sum2 = window2.Sum();
+            //        if (sum1 > 0 && sum2 == -1)
+            //        {
+            //            //now we can start reporting
+            //            Console.WriteLine($"{sum1} (N/A - no previous sum)");
+            //        }
+            //        else if (sum1 > 0 && sum2 > 0)
+            //        {
+            //            //continue reporting
+            //            if (sum1 < sum2)
+            //            {
+            //                Console.WriteLine($"{sum2} (decrease)");
+            //            }
+            //            else if (sum1 == sum2)
+            //            {
+            //                Console.WriteLine($"{sum2} (no change)");
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine($"{sum2} (increased)");
+            //                numOfIncreases++;
+            //            }
+            //        }
+            //    }
+            //}
 
-            Console.WriteLine($"The number of sums larger than the previous sum is {numOfIncreases}");
+            //Console.WriteLine($"The number of sums larger than the previous sum is {numOfIncreases}");
         }
     }
 }
