e7b728d1052650a82f0bd1cbc8cb3f21cf51410a
Rod Falanga <rfalanga@users.noreply.github.com>
2022-03-05 18:10:29 -0700

    making changes

--- AdventOfCode2021/AdventOfCode2021/Day2Prime.cs
+++ AdventOfCode2021/AdventOfCode2021/Day2Prime.cs
@@ -12,6 +12,39 @@ namespace AdventOfCode2021
         {
             //Should this be Day 3?
             Console.WriteLine("In Day 2 - Prime");
+
+            var lines = ReadDay2PrimeFile();
+
+            if (lines == null)
+            {
+                Console.WriteLine("Day 2 Prime file is empty");
+                return;
+            }
+
+            int horizontalPosition = 0;
+            int depth = 0;
+
+            //read through each line
+
+            //split the line using a space as the delimiter
+
+            //if the word is "forward", add the second value to horizontalPosition
+
+            //if the word is "down", add the second value to depth
+
+            //if the word is "up", subtract the second value from depth
         }
+
+        public static string[]? ReadDay2PrimeFile()
+        {
+            // inspired by: https://stackoverflow.com/questions/13762338/read-files-from-a-folder-present-in-project
+            string executionPath = Environment.CurrentDirectory;
+            var path = executionPath + @"\Data\Day2Prime.txt";
+
+            //Ingest the Day 2 Prime data
+
+            return File.ReadAllLines(path);
+        }
+
     }
 }
