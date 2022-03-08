b589fb68fb5116447141efb06567a5521a220d0b
Rod Falanga <rfalanga@users.noreply.github.com>
2022-03-07 19:00:46 -0700

    The Day3Binary array is wrong

--- AdventOfCode2021/AdventOfCode2021/Day3.cs
+++ AdventOfCode2021/AdventOfCode2021/Day3.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AdventOfCode2021.Common;
+using AdventOfCode2021.Models;
 
 namespace AdventOfCode2021
 {
@@ -11,7 +12,29 @@ namespace AdventOfCode2021
     {
         public static void DoPart1()
         {
-            //TODO: Need to work on this part
+            Console.WriteLine("In Day 3, Part 1");
+
+            var lines = ReadFileHelper.ReadTextFileReturnContents("Day3.txt");
+
+            //this is the tricky part
+            Day3Binary[12] day3Array = { new Day3Binary();
+                new Day3Binary();
+                new Day3Binary();
+                new Day3Binary();
+                new Day3Binary();
+                new Day3Binary();
+                new Day3Binary();
+                new Day3Binary();
+                new Day3Binary();
+                new Day3Binary();
+                new Day3Binary();
+                    new Day3Binary()
+                    };
+
+            foreach (var line in lines)
+            {
+                //
+            }
         }
     }
 }
