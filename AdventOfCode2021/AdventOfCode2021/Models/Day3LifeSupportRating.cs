140d03d138c7e738dba710cd9fce580cc61b6851
Rod Falanga <rfalanga@users.noreply.github.com>
2022-03-22 18:28:00 -0600

    finished class for Oxygen Generator and CO2 Scubber ratings

--- AdventOfCode2021/AdventOfCode2021/Models/Day3LifeSupportRating.cs
+++ AdventOfCode2021/AdventOfCode2021/Models/Day3LifeSupportRating.cs
@@ -1,13 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventOfCode2021.Models
+﻿namespace AdventOfCode2021.Models
 {
     public class Day3LifeSupportRating : Day3BinaryBase
     {
+        /// <summary>
+        /// Returns the most common value (0 or 1) in the current bit position.
+        /// If the number of 1's is greater than the number of 0's, then return "1".
+        /// If the number of 0's is greater than the number of 1's, then return "0".
+        /// If they are equal, then return "1".
+        /// </summary>
+        /// <returns></returns>
+        public string OxygenGeneratorRating()
+        {
+            if (numOf1s > numOf0s)
+            {
+                return "1";
+            }
+            else if (numOf1s < numOf0s)
+            {
+                return "0";
+            }
 
+            return "1";
+        }
+
+        /// <summary>
+        /// Returns the least common value (0 or 1) in the current bit position.
+        /// If the number of 1's is greater than the number of 0's, then return "1".
+        /// If the number of 0's is greater than the number of 1's, then return "0".
+        /// If they are equal, then return "0".
+        /// </summary>
+        /// <returns></returns>
+        public string CO2ScrubberRating()
+        {
+            if (numOf1s > numOf0s)
+            {
+                return "0";
+            }
+            else if (numOf1s < numOf0s)
+            {
+                return "1";
+            }
+
+            return "0";
+        }
     }
 }
